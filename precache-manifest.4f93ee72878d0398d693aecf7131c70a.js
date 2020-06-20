5891d16184a8bda20ed3095ff45a1b9eae83fade
remvok <remvok@gmail.com>
2020-06-20 10:39:52 +0300

    Updates

---
+++ precache-manifest.4f93ee72878d0398d693aecf7131c70a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ef93f596493df0805fe7be341ea1b5",
+    "url": "/test_task/index.html"
+  },
+  {
+    "revision": "843f914b18614d638789",
+    "url": "/test_task/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "9b6082973e76bb1bb583",
+    "url": "/test_task/static/css/main.17d4d144.chunk.css"
+  },
+  {
+    "revision": "843f914b18614d638789",
+    "url": "/test_task/static/js/2.676fbe58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test_task/static/js/2.676fbe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6082973e76bb1bb583",
+    "url": "/test_task/static/js/main.82175992.chunk.js"
+  },
+  {
+    "revision": "69c4828a9ca8a5973e62",
+    "url": "/test_task/static/js/runtime-main.ad3ea3fe.js"
+  }
+]);
